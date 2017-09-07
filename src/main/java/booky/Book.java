879b6639d43ec8496f276caa7503ce0133f76339
PAchilleos <procopis.achilleos@qa.com>
2017-09-07 13:22:21 +0100

    initial tests

---
+++ src/main/java/booky/Book.java
@@ -0,0 +1,21 @@
+package booky;
+
+public class Book {
+	
+	private int ID;
+	private String name;
+	private String genre;
+	private String author;
+	
+	
+	public Book(int iD, String name, String genre, String author) {
+		super();
+		ID = iD;
+		this.name = name;
+		this.genre = genre;
+		this.author = author;
+	}
+	
+	
+
+}
